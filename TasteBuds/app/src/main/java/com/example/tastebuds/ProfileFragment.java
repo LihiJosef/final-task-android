e615b7f3aa70a66feb6be8f278b882440324cb78
LihiJosef <lihiyosef6@gmail.com>
2023-01-16 18:04:42 +0200

    add user post list to profile

--- TasteBuds/app/src/main/java/com/example/tastebuds/ProfileFragment.java
+++ TasteBuds/app/src/main/java/com/example/tastebuds/ProfileFragment.java
@@ -1,14 +1,7 @@
 package com.example.tastebuds;
 
+import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.view.MenuProvider;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.lifecycle.Lifecycle;
-
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,11 +9,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.tastebuds.databinding.FragmentNewPostBinding;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.MenuProvider;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import com.example.tastebuds.databinding.FragmentProfileBinding;
+import com.example.tastebuds.model.Model;
 
 public class ProfileFragment extends Fragment {
     FragmentProfileBinding binding;
+    UserPostRecyclerAdapter adapter;
+    UserPostListFragmentViewModel viewModel;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,6 +50,56 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(inflater, container, false);
+
+        /*RecyclerView include:
+         * 1. Layout Manager
+         * 2. Adapter
+         * 3. ViewHolder
+         * 4. Row View*/
+        binding.recyclerView.setHasFixedSize(true);
+
+        // 1
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        // 2
+        adapter = new UserPostRecyclerAdapter(getLayoutInflater(), viewModel.getData());
+        binding.recyclerView.setAdapter(adapter);
+
+//        /*-> Handle row click in the activity*/
+//        adapter.setOnItemClickListener(new UserPostRecyclerAdapter.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(int pos) {
+//                Log.d("TAG", "row click handle in activity " + pos);
+//                Post post = viewModel.getData().get(pos);
+//                UserPostListFragmentDirections.ActionStudentListFragmentToBlueFragment action =  UserPostListFragmentDirections.actionStudentListFragmentToBlueFragment(post.getName());
+//                Navigation.findNavController(view).navigate(action);
+//            }
+//        });
+//
+//        // Define global action
+//        NavDirections action = StudentListFragmentDirections.actionGlobalAddStudentFragment();
+
         return binding.getRoot();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        viewModel = new ViewModelProvider(this).get(UserPostListFragmentViewModel.class);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        reloadData();
+    }
+
+    void reloadData() {
+//        binding.progressBar.setVisibility(View.VISIBLE);
+        Model.instance().getAllUserPosts((psList) -> {
+            viewModel.setData(psList);
+            adapter.setData(viewModel.getData());
+//            binding.progressBar.setVisibility(View.GONE);
+        });
     }
 }
---
+++ TasteBuds/app/src/main/java/com/example/tastebuds/UserPostRecyclerAdapter.java
@@ -0,0 +1,88 @@
+package com.example.tastebuds;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.tastebuds.model.Post;
+
+import java.util.List;
+
+// 3
+class UserPostViewHolder extends RecyclerView.ViewHolder{
+    ImageView postImage;
+    TextView locationTv;
+    TextView starsTv;
+    TextView reviewTv;
+    List<Post> data;
+
+    public UserPostViewHolder(@NonNull View itemView, UserPostRecyclerAdapter.OnItemClickListener listener, List<Post> data) {
+        super(itemView);
+        this.data = data;
+        postImage = itemView.findViewById(R.id.userpostlistrow_post_img);
+        locationTv = itemView.findViewById(R.id.userpostlistrow_location_tv);
+        starsTv = itemView.findViewById(R.id.userpostlistrow_stars_tv);
+        reviewTv = itemView.findViewById(R.id.userpostlistrow_review_tv);
+    }
+
+    public void bind(Post post, int pos){
+        locationTv.setText(post.getLocation());
+        starsTv.setText(post.getStars().toString() + "/5");
+        reviewTv.setText(post.getReview());
+
+//        if(post.getImageUrl() != "") {
+//            Picasso.get().load(post.getImageUrl()).placeholder(R.drawable.avatar).into(postImage);
+//        } else {
+//            postImage.setImageResource(R.drawable.avatar);
+//        }
+    }
+}
+
+public class UserPostRecyclerAdapter extends RecyclerView.Adapter<UserPostViewHolder>{
+    /*Set listener to catch view row click and handle in the activity ->*/
+    OnItemClickListener listener;
+    LayoutInflater inflater;
+    List<Post> data;
+
+    public static interface OnItemClickListener{
+        void onItemClick(int pos);
+    }
+
+    public void setData(List<Post> data){
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
+    public UserPostRecyclerAdapter(LayoutInflater inflater, List<Post> data){
+        this.inflater = inflater;
+        this.data = data;
+    }
+
+    void setOnItemClickListener(OnItemClickListener listener){
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public UserPostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // 4
+        View view = inflater.inflate(R.layout.userpost_list_row, parent, false);
+        return new UserPostViewHolder(view, listener, data);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserPostViewHolder holder, int position) {
+        Post post = data.get(position);
+        holder.bind(post, position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+}
