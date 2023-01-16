30641e09a1c6890fdd380586ddfea44de76b7510
LihiJosef <lihiyosef6@gmail.com>
2023-01-16 17:33:19 +0200

    create fragment profile xml and user post xml

--- TasteBuds/app/src/main/java/com/example/tastebuds/FeedRecyclerAdapter.java
+++ TasteBuds/app/src/main/java/com/example/tastebuds/FeedRecyclerAdapter.java
@@ -1,10 +1,8 @@
 package com.example.tastebuds;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
